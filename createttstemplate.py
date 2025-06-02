4e2928832bf2e06abe668d3dfba1c558da4c9db3
cdr <58159483+cdrg@users.noreply.github.com>
2025-06-01 19:25:36 -0600

    Add functionality to update existing templates with new files

--- createttstemplate.py
+++ createttstemplate.py
@@ -1,13 +1,14 @@
 """
-Create a json TTS audio file list template with default entries from an existing TTS directory.
-Should only needed to be run once, when creating a template for the first time.
-Template should be fixed as necessary by hand after creation.
+Create or update a json TTS audio file list template with default entries from an existing TTS directory.
+
+Template file entires can be improved by hand as necessary after creation of defaults.
 
 ---
 
 template item json format: {path, tts_text, ssml_text}
 
 path: the relative file path including all folders, filename, extension (.mp3)
+    file name should start with the full canonical name of the item, class, etc, then any modifiers  
 tts_text: the actual text to be TTS read, in plain text; for in-game clarity, 
     can be completely different than the filename
 ssml_text: (optional) SSML marked up text, empty string if none
@@ -18,63 +19,92 @@ import os
 import argparse
 import json
 
-
 def main():
     '''
-    Create a json TTS audio file list template with default entries from an existing TTS directory.
+    Create a json TTS audio file list template with default entries from files in an existing TTS 
+    directory, or update an existing template file with new entries from new files.
 
     Returns:
-        exit_val (int): 0 on success, 1 on error
+        (int): 0 on success, 1 on error
     '''
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--directory", help="the input directory to crawl", default=os.getcwd())
     parser.add_argument("-f", "--file", help="the name of the output template json file", default="template.json")
-    args = parser.parse_args()
+    args: argparse.Namespace = parser.parse_args()
 
     if not os.path.exists(args.directory):
         print(f"Error: directory '{args.directory}' does not exist.")
         return 1
 
-    template = []
+    template: list[dict[str, str]] = []
+    file_exists: bool = os.path.isfile(args.file)
+    if file_exists:
+        try:
+            with open(args.file, encoding="utf-8", ) as f:
+                template = json.load(f)
+        except (json.JSONDecodeError, UnicodeDecodeError) as e:
+            print(f"{type(e)}: {e}")
+            return 1
 
-    entry_count = 0
+    created_count: int = 0
+    skipped_count: int = 0
     # create template data structure from existing directory structure
     for root, _, files in os.walk(args.directory, topdown=False):
         for filename in files:
-            if filename.endswith(".mp3"):
-                # path relative to sounds dir, remove leading path separator if present
-                entry_path = os.path.join(root.split("sounds")[1], filename).removeprefix("/")
+            # skip non-.mp3 files
+            if not filename.endswith(".mp3"):
+                continue
+            # skip files in voicelines dir since they're not TTS files
+            elif "voicelines" in root:
+                continue
 
-                entry_tts_text = filename.removesuffix(".mp3")
+            # get path relative to sounds dir, remove leading path separator if present
+            entry_path: str = os.path.join(root.split("sounds")[1], filename).removeprefix("/")
+
+            # skip if entry path already exists in template list
+            if any(entry["path"] == entry_path for entry in template):
+                skipped_count += 1
+                continue
+            else:
+                entry_tts_text: str = filename.removesuffix(".mp3")
+
                 # move 'rare' to front of text if it ends with 'rare'
                 if entry_tts_text.endswith(" rare"):
                     entry_tts_text = "rare " + entry_tts_text.removesuffix(" rare")
-                 # move 'magic' to front of text if it ends with 'magic'
+                # move 'magic' to front of text if it ends with 'magic'
                 if entry_tts_text.endswith(" magic"):
                     entry_tts_text = "magic " + entry_tts_text.removesuffix(" magic")
                 # remove redundant currency prefixes and suffixes such as "orb of " or " orb"
-                entry_tts_text = entry_tts_text.replace("orb of ", "")
-                entry_tts_text = entry_tts_text.replace("scroll of ", "")
-                entry_tts_text = entry_tts_text.replace(" orb", "")
+                if "currency/" in entry_path:
+                    if entry_tts_text.startswith("orb of "):
+                        entry_tts_text = entry_tts_text.removeprefix("orb of ")
+                    elif " orb" in entry_tts_text:
+                        entry_tts_text = entry_tts_text.replace(" orb", "")
+                    elif entry_tts_text.startswith("scroll of "):
+                        entry_tts_text = entry_tts_text.removeprefix("scroll of ")
+                    elif entry_tts_text.startswith("blacksmiths whetstone"):
+                        entry_tts_text = entry_tts_text.removeprefix("blacksmiths ")
+                    elif entry_tts_text.startswith("armourers scrap"):
+                        entry_tts_text = entry_tts_text.replace(" scrap", "")
 
-                entry_ssml_text = ""
-                # create fast rate prosidy ssml if tts text is multiple words
-                if " " in entry_tts_text:
+                entry_ssml_text: str = ""
+                # if tts text is multiple words or is at least 10 characters, set fast rate prosody ssml
+                if " " in entry_tts_text or len(entry_tts_text) >= 10:
                     entry_ssml_text = "<prosody rate='fast'>" + entry_tts_text + "</prosody>"
-                # fix pronunciation if (ssml) text contains '1h' or '2h'
+                # literal pronunciation if text contains '1h' or '2h' (otherwise advanced TTS reads "hour")
                 if "1h" in entry_ssml_text:
                     entry_ssml_text = entry_ssml_text.replace("1h", "<say-as interpret-as='characters'>1h</say-as>")
                 if "2h" in entry_ssml_text:
                     entry_ssml_text = entry_ssml_text.replace("2h", "<say-as interpret-as='characters'>2h</say-as>")
 
-                template.append((entry_path, entry_tts_text, entry_ssml_text))
-                entry_count += 1
-                if entry_count > 1:
+                template.insert(created_count+skipped_count+1, {"path": entry_path, "tts_text": entry_tts_text, "ssml_text": entry_ssml_text})
+                created_count += 1
+                if created_count > 1:
                     print("\033[1A", end="\x1b[2K")
-                print(f"Read entry {entry_count}: {template[-1]}", flush=True)
+                print(f"Created entry {created_count}: {template[created_count+skipped_count-1]}", flush=True)
 
-    # create json from template data structure
-    template_json = json.dumps(template, indent=4)
+    # create json string from template data structure
+    template_json: str = json.dumps(template, indent=4)
 
     # output json to file
     try:
@@ -84,7 +114,11 @@ def main():
         print(f"{type(e)}: {e}")
         return 1
 
-    print(f"Successfully created template file '{args.file}' with {entry_count} entries.")
+    if file_exists:
+        print(f"Updated existing file '{args.file}'. {created_count} new entries created, "
+              f"{skipped_count} existing entries skipped.")
+    else:
+        print(f"Successfully created template file '{args.file}' with {created_count} entries.")
     return 0
 
 if __name__ == "__main__":
